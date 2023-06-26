d2aa420683d1ecd2f3c30efaa5a6614a9a7c905f
nathansilvavar <nathan.silva@varsolutions.com.br>
2023-06-26 06:55:02 -0300

    update

--- aulainvertida/script.js
+++ aulainvertida/script.js
@@ -1,3 +1,4 @@
+
 window.onscroll = () => {
   if (document.documentElement.scrollTop > window.innerHeight) {
     console.log(window.innerHeight)
@@ -33,13 +34,13 @@ var casasJson = [
   },
   {
     onClick: "",
-    cover: "./img/img2.png",
+    cover: "./img/serra.png",
     titulo: "Serra Negra, Brasil",
     preco: "R$750 noite"
   },
   {
     onClick: "",
-    cover: "./img/img3.png",
+    cover: "./img/ubatuba.jpeg",
     titulo: "Ubatuba, Brasil",
     preco: "R$650 noite"
   },
@@ -91,7 +92,7 @@ function abrirfiltro1() {
 
 
 }
-function fecharfiltro1(){
+function fecharfiltro1() {
   document.getElementById("filtrocidade").style.display = "none"
 
 }
@@ -159,9 +160,161 @@ function Trocarlogin() {
 function IrParaHome() {
   window.location.href = './index.html'
 }
-function Irparapagamento(){
-  window.location.href = './pagamento.html'
+function Irparapagamento() {
+  let datain = document.getElementById("resultadocheckin").innerText
+  let dataout = document.getElementById("resultadocheckout").innerText
+  let total = Number(document.getElementById("resultado").innerText.replace("R$", '').replace(",00", ""))
+  let hospedes = Number(document.getElementById("hospedes").innerText)
+  let limpeza = Number(document.getElementById("taxalimp").innerText.replace("R$", '').replace(",00", ""))
+
+  if (datain != "" || dataout != "") {
+    let pagamentojson = {
+      datain: datain,
+      dataout: dataout,
+      total: total,
+      hospedes: hospedes,
+      limpeza: limpeza
+    }
+
+    window.localStorage.setItem("pagamento", JSON.stringify(pagamentojson))
+
+    window.location.href = './pagamento.html'
+  }
+  else
+    alert("Por favor, preencha os dias de Check-in e Checkout")
 }
-function IrParaDetalhes(){
+function IrParaDetalhes() {
   window.location.href = './casa.html'
 }
+
+function data(request) {
+  document.getElementById("resultadocheckin").innerText = `${request.innerText}/06/2023`
+  if (document.getElementById("resultadocheckout").innerText != '') {
+    let datain = Number(document.getElementById("resultadocheckin").innerText.split("/")[0])
+    let dataout = Number(document.getElementById("resultadocheckout").innerText.split("/")[0])
+    if (datain >= dataout) {
+      alert("A data de Check-in não pode ser maior do que a data de Checkout")
+      document.getElementById("resultadocheckout").innerText = ``
+    }
+    else {
+      let dias = dataout - datain
+      let limpeza = document.getElementById("taxalimp").innerText.replace("R$", '').replace(",00", "")
+      document.getElementById("resultado").innerText = `R$${dias * 650 + Number(limpeza)},00`
+    }
+  }
+}
+function data2(request) {
+  document.getElementById("resultadocheckout").innerText = `${request.innerText}/06/2023`
+
+  if (document.getElementById("resultadocheckin").innerText != '') {
+    let datain = Number(document.getElementById("resultadocheckin").innerText.split("/")[0])
+    let dataout = Number(document.getElementById("resultadocheckout").innerText.split("/")[0])
+    if (datain >= dataout) {
+      alert("A data de Check-in não pode ser maior do que a data de Checkout")
+      document.getElementById("resultadocheckout").innerText = ``
+    }
+    else {
+      let dias = dataout - datain
+      let limpeza = document.getElementById("taxalimp").innerText.replace("R$", '').replace(",00", "")
+      document.getElementById("resultado").innerText = `R$${dias * 650 + Number(limpeza)},00`
+    }
+  }
+
+
+}
+function fecharCheckin() {
+  var calendario = document.getElementById("checkin")
+  calendario.style.display = 'none'
+}
+function CriarCalendario() {
+  var calendario = document.getElementById("checkin")
+  var calendariotext = ''
+  var calendariotext2 = ''
+  for (let i = 1; i <= 31; i++) {
+    calendariotext += `<li onclick='data(this)'>${i}</li>`
+    // if (i % 7 == 0) {
+    //   calendariotext2 += `<ul>${calendariotext}</ul>`
+    //   calendariotext = ''
+    // }
+  }
+  calendario.innerHTML = `<ul>${calendariotext}</ul>`
+  calendario.style.display = 'flex'
+}
+
+function fecharCheckin2() {
+  var calendario = document.getElementById("checkin2")
+  calendario.style.display = 'none'
+}
+function CriarCalendario2() {
+  var calendario = document.getElementById("checkin2")
+  var calendariotext = ''
+  var calendariotext2 = ''
+  for (let i = 1; i <= 31; i++) {
+    calendariotext += `<li onclick='data2(this)'>${i}</li>`
+    // if (i % 7 == 0) {
+    //   calendariotext2 += `<ul>${calendariotext}</ul>`
+    //   calendariotext = ''
+    // }
+  }
+  calendario.innerHTML = `<ul>${calendariotext}</ul>`
+  calendario.style.display = 'flex'
+}
+function AbrirHospedes() {
+  var modal = document.getElementById("checkin3")
+  modal.style.display = 'block'
+}
+function fecharCheckin3() {
+  var modal = document.getElementById("checkin3")
+  modal.style.display = 'none'
+}
+function abrirparcelas() {
+  var modal = document.getElementById("checkin5")
+  modal.style.display = 'flex'
+}
+function fecharCheckin5() {
+  var modal = document.getElementById("checkin5")
+  modal.style.display = 'none'
+}
+function somarhospedes() {
+  document.getElementById("hospedes").innerText = Number(document.getElementById("hospedes").innerText) + 1
+  document.getElementById("resultadohospedes").innerText = document.getElementById("hospedes").innerText + " hóspedes"
+  document.getElementById("taxalimp").innerText = `R$${Math.round(218.18 + (218.18 * (0.1 * Number(document.getElementById("hospedes").innerText))))},00`
+  let datain = Number(document.getElementById("resultadocheckin").innerText.split("/")[0])
+  let dataout = Number(document.getElementById("resultadocheckout").innerText.split("/")[0])
+
+  let dias = dataout - datain
+  let limpeza = document.getElementById("taxalimp").innerText.replace("R$", '').replace(",00", "")
+  document.getElementById("resultado").innerText = `R$${dias * 650 + Number(limpeza)},00`
+
+
+}
+function menarhospedes() {
+  if (Number(document.getElementById("hospedes").innerText) - 1 >= 1)
+    document.getElementById("hospedes").innerText = Number(document.getElementById("hospedes").innerText) - 1
+  document.getElementById("resultadohospedes").innerText = document.getElementById("hospedes").innerText + " hóspedes"
+  document.getElementById("taxalimp").innerText = `R$${Math.round(218.18 + (218.18 * (0.1 * Number(document.getElementById("hospedes").innerText))))},00`
+  let datain = Number(document.getElementById("resultadocheckin").innerText.split("/")[0])
+  let dataout = Number(document.getElementById("resultadocheckout").innerText.split("/")[0])
+
+  let dias = dataout - datain
+  let limpeza = document.getElementById("taxalimp").innerText.replace("R$", '').replace(",00", "")
+  document.getElementById("resultado").innerText = `R$${dias * 650 + Number(limpeza)},00`
+}
+
+function validarparcela() {
+
+  const radioSelecionado = document.querySelector('input[name="radio23"]:checked').value
+  let total = pagamento.total
+  if (radioSelecionado > 3)
+    total = total * 1.2
+
+  document.getElementById("total").innerText = `${radioSelecionado}x ${(total / radioSelecionado).toLocaleString('pt-br', { style: 'currency', currency: 'BRL' })}`
+
+}
+function EditarInformacoes(){
+  window.location.href = "./casa.html"
+}
+function Alertar(texto){
+  alert(texto)
+}
+
