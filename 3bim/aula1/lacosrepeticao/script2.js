e9987967e59930dfc59b703ce6491dbca2be45cc
nathansilvavar <nathan.silva@varsolutions.com.br>
2023-09-04 09:32:07 -0300

    update

---
+++ 3bim/aula1/lacosrepeticao/script2.js
@@ -0,0 +1,11 @@
+let i = 0
+let fatorial = 1
+
+while(i <= 5){
+    i++;
+    fatorial *= i 
+    console.log(`Passo - ${i}`);    
+    console.log(`Fatorial - ${fatorial}`);    
+}
+
+console.log("FIM");
