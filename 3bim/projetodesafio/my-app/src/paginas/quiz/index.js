3bb24ffc9d6a12613b899f7826e9c6762a422779
nathansilvavar <nathan.silva@varsolutions.com.br>
2023-09-28 20:48:20 -0300

    finalizando desafio interface Web

--- 3bim/projetodesafio/my-app/src/paginas/quiz/index.js
+++ 3bim/projetodesafio/my-app/src/paginas/quiz/index.js
@@ -1,15 +1,19 @@
 import React, { useState, useEffect } from "react";
 import api from "../../services/api";
-import { Link, useParams } from "react-router-dom";
+import { Link, json, useNavigate, useParams } from "react-router-dom";
 import { NavBar } from "../../componentes/navbar";
 import { BsFillArrowLeftCircleFill } from "react-icons/bs";
 import { toast } from 'react-toastify';
 import './quiz.css'
 export const Quiz = () => {
+    const [userName, setUserName] = useState("");
     const [questions, setQuestions] = useState([]);
-    const [rightQuestions, setrightQuestions] = useState(0);
+    const [rightQuestions, setRightQuestions] = useState(0);
     const [currentQuestion, setCurrentQuestion] = useState(0);
     const [selectedOption, setSelectedOption] = useState(null);
+    const [quizStarted, setQuizStarted] = useState(false);
+
+    const navigate = useNavigate();
 
     useEffect(() => {
 
@@ -32,48 +36,48 @@ export const Quiz = () => {
                 options: ["Charles Dickens", "William Shakespeare", "Jane Austen", "Leo Tolstoy"],
                 correctAnswer: "William Shakespeare",
             },
-            {
-                id: 4,
-                question: "Qual é o símbolo químico do ouro?",
-                options: ["Au", "Ag", "Fe", "Hg"],
-                correctAnswer: "Au",
-            },
-            {
-                id: 5,
-                question: "Qual é o rio mais longo do mundo?",
-                options: ["Amazonas", "Nilo", "Mississippi", "Yangtze"],
-                correctAnswer: "Nilo",
-            },
-            {
-                id: 6,
-                question: "Quantos planetas fazem parte do nosso sistema solar?",
-                options: ["5", "7", "8", "9"],
-                correctAnswer: "8",
-            },
-            {
-                id: 7,
-                question: "Qual é o maior animal do mundo?",
-                options: ["Elefante", "Baleia Azul", "Girafa", "Rinoceronte"],
-                correctAnswer: "Baleia Azul",
-            },
-            {
-                id: 8,
-                question: "Qual é o elemento mais abundante na crosta terrestre?",
-                options: ["Ferro", "Oxigênio", "Silício", "Alumínio"],
-                correctAnswer: "Oxigênio",
-            },
-            {
-                id: 9,
-                question: "Quem pintou a 'Mona Lisa'?",
-                options: ["Pablo Picasso", "Vincent van Gogh", "Leonardo da Vinci", "Michelangelo"],
-                correctAnswer: "Leonardo da Vinci",
-            },
-            {
-                id: 10,
-                question: "Qual é o maior deserto do mundo?",
-                options: ["Sahara", "Atacama", "Gobi", "Antártica"],
-                correctAnswer: "Antártica",
-            },
+            // {
+            //     id: 4,
+            //     question: "Qual é o símbolo químico do ouro?",
+            //     options: ["Au", "Ag", "Fe", "Hg"],
+            //     correctAnswer: "Au",
+            // },
+            // {
+            //     id: 5,
+            //     question: "Qual é o rio mais longo do mundo?",
+            //     options: ["Amazonas", "Nilo", "Mississippi", "Yangtze"],
+            //     correctAnswer: "Nilo",
+            // },
+            // {
+            //     id: 6,
+            //     question: "Quantos planetas fazem parte do nosso sistema solar?",
+            //     options: ["5", "7", "8", "9"],
+            //     correctAnswer: "8",
+            // },
+            // {
+            //     id: 7,
+            //     question: "Qual é o maior animal do mundo?",
+            //     options: ["Elefante", "Baleia Azul", "Girafa", "Rinoceronte"],
+            //     correctAnswer: "Baleia Azul",
+            // },
+            // {
+            //     id: 8,
+            //     question: "Qual é o elemento mais abundante na crosta terrestre?",
+            //     options: ["Ferro", "Oxigênio", "Silício", "Alumínio"],
+            //     correctAnswer: "Oxigênio",
+            // },
+            // {
+            //     id: 9,
+            //     question: "Quem pintou a 'Mona Lisa'?",
+            //     options: ["Pablo Picasso", "Vincent van Gogh", "Leonardo da Vinci", "Michelangelo"],
+            //     correctAnswer: "Leonardo da Vinci",
+            // },
+            // {
+            //     id: 10,
+            //     question: "Qual é o maior deserto do mundo?",
+            //     options: ["Sahara", "Atacama", "Gobi", "Antártica"],
+            //     correctAnswer: "Antártica",
+            // },
         ];
         setQuestions(dummyQuestions);
     }, []);
@@ -85,26 +89,43 @@ export const Quiz = () => {
     const handleNextQuestion = () => {
         toast.dismiss();
 
-        const currentQues = questions[currentQuestion];
-        if (selectedOption === currentQues.correctAnswer) {
-
-            setrightQuestions(rightQuestions + 1)
-            toast.success("Resposta correta!",{
+        if (!quizStarted) {
+            setQuizStarted(true);
+        }
+        if (selectedOption === null || selectedOption === undefined) {
+            toast.error("Por favor, selecione uma opção antes de avançar!", {
                 position: "top-center",
-
-            })
-        } else {
-            toast.error("Resposta incorreta!",{
-                position: "top-center",
-
             });
+            return; 
         }
 
+        const currentQues = questions[currentQuestion];
+        if (selectedOption === currentQues.correctAnswer) {
+            setRightQuestions(rightQuestions + 1);
+            toast.success("Resposta correta!", {
+                position: "top-center",
+            });
+        } else {
+            toast.error("Resposta incorreta!", {
+                position: "top-center",
+            });
+        }
+        const total = currentQuestion + 1;
         if (currentQuestion < questions.length - 1) {
             setCurrentQuestion(currentQuestion + 1);
             setSelectedOption(null);
         } else {
-            
+            let ranking = window.localStorage.getItem("ranking");
+            if (ranking == null)
+                ranking = [];
+            else
+                ranking = JSON.parse(ranking);
+            ranking.push({
+                name: userName,
+                points: total
+            })
+            window.localStorage.setItem("ranking", JSON.stringify(ranking));
+            navigate("/ranking")
             toast.success("Quiz Concluído", {
                 position: "top-center",
                 autoClose: 5000,
@@ -114,9 +135,10 @@ export const Quiz = () => {
                 draggable: true,
                 progress: undefined,
                 theme: "colored",
-            })
+            });
         }
     };
+
 
     return (
         <>
@@ -130,24 +152,36 @@ export const Quiz = () => {
             </div>
 
             <div className="quiz-container">
-                {currentQuestion < questions.length && (
-                    <div className="question-container">
-                        <h2>Pergunta {currentQuestion + 1}</h2>
-                        <p>{questions[currentQuestion].question}</p>
-                        <div className="options-container">
-                            {questions[currentQuestion].options.map((option, index) => (
-                                <div
-                                    key={index}
-                                    className={`option ${selectedOption === option ? "selected" : ""}`}
-                                    onClick={() => handleOptionSelect(option)}
-                                >
-                                    {option}
-                                </div>
-                            ))}
-                        </div>
-                        <button onClick={handleNextQuestion}>Próxima Pergunta</button>
+                {!quizStarted ? (
+                    <div className="start-quiz">
+                        <input
+                            type="text"
+                            placeholder="Digite seu nome"
+                            value={userName}
+                            onChange={(e) => setUserName(e.target.value)}
+                        />
+                        <button onClick={() => setQuizStarted(true)}>Começar o Quiz</button>
                     </div>
-                )}
+                ) :
+                    currentQuestion < questions.length && (
+                        <div className="question-container">
+                            <h2>Pergunta {currentQuestion + 1}</h2>
+                            <p>{questions[currentQuestion].question}</p>
+                            <div className="options-container">
+                                {questions[currentQuestion].options.map((option, index) => (
+                                    <div
+                                        key={index}
+                                        className={`option ${selectedOption === option ? "selected" : ""}`}
+                                        onClick={() => handleOptionSelect(option)}
+                                    >
+                                        {option}
+                                    </div>
+                                ))}
+                            </div>
+                            <button onClick={handleNextQuestion}>Próxima Pergunta</button>
+                        </div>
+                    )
+                }
             </div>
         </>
     );
