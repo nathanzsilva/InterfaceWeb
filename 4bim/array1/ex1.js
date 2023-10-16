7b509e69905e018677d81121f38c63ad63fa85fb
Nathan Do Carmo <nathan.silva26@gmail.com>
2023-10-16 11:07:59 -0300

    criacao de array em js

---
+++ 4bim/array1/ex1.js
@@ -0,0 +1,5 @@
+var alunos_informatica = ["nathan", "eu", "andre","andre2",];
+
+console.log(alunos_informatica.sort());
+console.log(alunos_informatica.reverse());
+
---
+++ 4bim/array1/ex2.js
@@ -0,0 +1,18 @@
+
+var lista = ["arroz", "feijão", "açúcar", "café",];
+
+
+//Método .push()
+// lista.push("elemento adicionado", "segunddo elemento adicionado");
+
+// console.log("Depois de incrementar os elementos");
+// console.log(lista);
+
+// Método .pop()
+// lista.pop();
+
+// console.log("Depois de remover o último elemento");
+// console.log(lista);
+
+//Método .toString()    
+console.log(lista.toString());
